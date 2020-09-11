243cccf0981e659ce88082a5d41dcc23182854a9
Алексей <zotov.alexei.1@yandex.ru>
2020-09-11 22:10:58 +0300

    second screen

---
+++ app/src/main/java/com/example/vk_layouts/DonationTypeActivity.java
@@ -0,0 +1,14 @@
+package com.example.vk_layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DonationTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_screen);
+    }
+}
--- app/src/main/java/com/example/vk_layouts/MainActivity.java
+++ app/src/main/java/com/example/vk_layouts/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.vk_layouts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_screen);
+
+        Button create = findViewById(R.id.create_donation);
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DonationTypeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
